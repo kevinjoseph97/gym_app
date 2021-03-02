16dbe961f5d33f18168e70716792a1f3f5c55acd
Kevin <kevinjoseph97@gmail.com>
2021-03-02 14:46:16 -0500

    adds forms get/post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,14 +17,30 @@ class ApplicationController < Sinatra::Base
         erb :welcome
     end
 
+    #creating a new workout 
+    get '/welcome/new' do 
+        erb :new_workout
+    end
+
+
+    post '/welcome' do 
+        @workout = params
+        erb :workout
+    end
+
     #set up dynamic routes to see individual links from the welcome page
     get '/welcome/:id' do 
         body_parts = [{name: "Arms", id: 1}, {name: "Legs", id: 2}, {name: "Chest", id: 3}]
         @workout = body_parts.find {|i| i[:id] == params[:id].to_i}
 
         erb :workout
-
     end
+
+    
+
+   
+
+
 
 
 
