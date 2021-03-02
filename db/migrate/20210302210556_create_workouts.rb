c748ad7e00840e5f2f5bc49c5f74737e814e9e95
Kevin <kevinjoseph97@gmail.com>
2021-03-02 16:14:16 -0500

    adds workout table

--- db/migrate/20210302210556_create_workouts.rb
+++ db/migrate/20210302210556_create_workouts.rb
@@ -1,4 +1,13 @@
 class CreateWorkouts < ActiveRecord::Migration[5.2]
   def change
+    create_table :workouts do |w|
+      w.string :exercise1
+      w.string :exercise2
+      w.string :exercise3
+      w.string :exercise4
+      w.integer :user_id
+    end
+
+
   end
 end
