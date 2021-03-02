72d1ede19acc81718c94bbbb1de343bd2ce4215d
Kevin <kevinjoseph97@gmail.com>
2021-03-02 14:03:07 -0500

    adds views tables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,29 @@
 class ApplicationController < Sinatra::Base
 
     configure do
-        set(:views, 'app/views')
-        set :public_folder, 'public'
+        set(:views, 'app/views') #telling controller where to look for the views folder 
+        set :public_folder, 'public' 
     end
 
     get '/' do 
-        erb :home
+        @name = "Kevin"
+        erb :home 
+    end
+
+    #we need to see be able to show user all their workouts
+    get '/welcome' do 
+        @workouts = ["Arms", "Legs", "Chest"]
+        
+        erb :welcome
+    end
+
+    #set up dynamic routes to see individual links from the welcome page
+    get '/welcome/:id' do 
+        body_parts = [{name: "Arms", id: 1}, {name: "Legs", id: 2}, {name: "Chest", id: 3}]
+        @workout = body_parts.find {|i| i[:id] == params[:id].to_i}
+
+        erb :workout
+
     end
 
 
